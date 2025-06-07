f50b958b22193f2e7b0c39801b0dd08feeab9060
Sergio0831 <sergejs.ivcenko@gmail.com>
2025-06-07 12:52:36 +0100

    Modified middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -40,12 +40,9 @@ export default auth(req => {
 	return undefined;
 });
 
-// Optionally, don't invoke Middleware on some paths
 export const config = {
 	matcher: [
-		// Skip Next.js internals and all static files, unless found in search params
-		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-		// Always run for API routes
-		'/(api|trpc)(.*)'
+		// Skip Next.js internals, static files, and NextAuth routes
+		'/((?!_next|api/auth|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)'
 	]
 };
